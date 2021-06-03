56c19bfb02657c8a1e3a7464f6072921ffd4298a
Adam Snaider <adsnaider@gmail.com>
2021-06-03 13:25:31 -0700

    Created execute trait for different types.

--- src/lib.rs
+++ src/lib.rs
@@ -6,43 +6,58 @@
 //!   jumping.
 //!   * Wasm output is returned.
 
-use std::ops::Deref;
-
 use wasm_parse::wasm::func::Func;
 use wasm_parse::wasm::instr::{Instr, NumericInstr};
 use wasm_parse::wasm::module::Module;
 use wasm_parse::wasm::types::{NumType, ValType};
 
-pub fn execute(module: &Module, inputs: &[u32]) -> Vec<u32> {
+#[derive(Copy, Clone, Debug)]
+pub enum Val {
+    I32(u32),
+    I64(u64),
+    F32(f32),
+    F64(f64),
+}
+
+pub struct Context<'a> {
+    pub module: &'a Module,
+}
+
+trait Execute {
+    fn execute(&self, stack: &mut Vec<Val>, context: &Context);
+}
+
+pub fn execute_module(module: &Module, inputs: &[Val]) -> Vec<Val> {
     assert!(module.start.is_some(), "Module must have a start function");
+    let context = Context { module };
     let mut stack = Vec::new();
-    stack.extend(inputs.iter());
-    execute_func(
-        &module.funcs[*module.start.as_ref().unwrap().func.0.deref() as usize],
-        &mut stack,
-        &module,
-    );
+    stack.extend(inputs);
+    module.funcs[*module.start.as_ref().unwrap().func.0 as usize].execute(&mut stack, &context);
     stack
 }
 
-fn execute_func(func: &Func, stack: &mut Vec<u32>, module: &Module) {
-    assert!(
-        stack.len() >= func.locals.len(),
-        "Mismatch between params and arguments passed."
-    );
-    assert!(func.locals.iter().all(|x| match x {
-        ValType::Num(NumType::I32) => true,
-        _ => false,
-    }));
-    for instr in &func.body.instr {
-        execute_instr(&instr, stack);
+impl Execute for Func {
+    fn execute(&self, stack: &mut Vec<Val>, context: &Context) {
+        assert!(
+            stack.len() >= self.locals.len(),
+            "Mismatch between params and arguments passed."
+        );
+        assert!(self.locals.iter().all(|x| match x {
+            ValType::Num(NumType::I32) => true,
+            _ => false,
+        }));
+        for instr in &self.body.instr {
+            instr.execute(stack, context);
+        }
     }
 }
 
-fn execute_instr(instr: &Instr, stack: &mut Vec<u32>) {
-    match instr {
-        Instr::Numeric(NumericInstr::I32Const(ref x)) => stack.push(x.0),
-        instr => unimplemented!("Can't execute instruction {:?}", instr),
+impl Execute for Instr {
+    fn execute(&self, stack: &mut Vec<Val>, _context: &Context) {
+        match self {
+            Instr::Numeric(NumericInstr::I32Const(ref x)) => stack.push(Val::I32(x.0)),
+            instr => unimplemented!("Can't execute instruction {:?}", instr),
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use wasm_parse::wasm::module::Module;
-use wasm_runtime::execute;
+use wasm_runtime::execute_module;
 
 fn main() {
     let module = Module::from_binary(
         std::fs::read("../../wasm-examples/const.wasm").expect("Can't read wasm file."),
     );
-    println!("Execution of wasm returned: {:?}", execute(&module, &[]));
+    println!(
+        "Execution of wasm returned: {:?}",
+        execute_module(&module, &[])
+    );
 }
