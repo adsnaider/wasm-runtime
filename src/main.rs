0c0dfa23694308707c7c8cfdde392c76e5cdaa55
Adam Snaider <adsnaider@gmail.com>
2021-06-03 21:18:32 -0700

    A bit of reorganization.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use wasm_parse::wasm::module::Module;
 use wasm_runtime::execute_module;
 
 fn main() {
-    let module = Module::from_binary(
-        std::fs::read("../../wasm-examples/add.wasm").expect("Can't read wasm file."),
-    );
+    let filename: String = std::env::args()
+        .skip(1)
+        .next()
+        .expect("Missing wasm filename to execute.");
+    let module = Module::from_binary(std::fs::read(filename).expect("Can't read wasm file."));
     println!(
         "Execution of wasm returned: {:?}",
         execute_module(&module, &[])
