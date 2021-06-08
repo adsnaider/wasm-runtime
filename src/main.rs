574d2d35384858f7c20340b0b6e1e8c9c2062fa2
Adam Snaider <adsnaider@gmail.com>
2021-06-07 17:38:17 -0700

    Updated submodule and fixed print.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use wasm_runtime::Val;
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).take(2).collect();
-    println!("Func to execute: {}", args[1]);
     let module = Module::from_binary(std::fs::read(&args[0]).expect("Can't read wasm file."));
     println!(
         "Execution of wasm returned: {:?}",
